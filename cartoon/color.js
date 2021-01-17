a33b8410878970aab04499852012148be393e89e
LeeJiHey <gx530@naver.com>
2021-01-17 23:37:58 +0900

    1.17 stage

---
+++ cartoon/color.js
@@ -0,0 +1,40 @@
+var Links = {
+    setColor: function(color){
+        // var target2 = document.querySelectorAll('a');
+        // for (i = 0; i < target2.length; i++) {
+        //     target2[i].style.color = color;
+        // }
+        $('a').css('color', color);
+    }
+}
+
+var Body = {
+    setColor: function(color){
+        //document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+    },
+    setBackgroundColor: function(color){
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
+    }
+}
+
+
+
+function nightDayHandler(self) { 
+    var target = document.querySelector('body');
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+        Links.setColor('blueviolet');
+    }
+    else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        target.style.backgroundColor = 'white';
+        target.style.color = 'black';
+        self.value = 'night';
+        Links.setColor('blue');
+    }
+}
